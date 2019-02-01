621e0b592f6097ccf85431f60e4d4b65a8acdf15
Nikola Milekic <nikolamilekic@gmail.com>
2019-02-01 21:10:33 +0100

    Make sure Secret tolerates memory locking errors

--- src/FsSodium/Secret.fs
+++ src/FsSodium/Secret.fs
@@ -7,21 +7,15 @@ open Milekic.YoLo
 type Secret(secret) =
     static let secretName = nameOf <@ instanceOf<Secret>.Secret @>
     let secretLength = Array.length secret
-    do
-        let result = Interop.sodium_mlock(secret, secretLength)
-        if result <> 0 then
-            Interop.sodium_memzero(secret, secretLength)
-            failwith "Failed to lock memory."
+    do Interop.sodium_mlock(secret, secretLength) |> ignore
+    let dispose () = Interop.sodium_munlock(secret, secretLength) |> ignore
+
     member __.Secret = secret
     new(info : SerializationInfo, _ : StreamingContext) =
         let secret = info.GetValue (secretName, typeof<byte[]>) :?> byte[]
         new Secret(secret)
     interface IDisposable with
-        member __.Dispose() =
-            let result = Interop.sodium_munlock(secret, secretLength)
-            if result <> 0 then
-                Interop.sodium_memzero(secret, secretLength)
-                failwith "Failed to unlock memory."
-    override this.Finalize() = (this :> IDisposable).Dispose()
+        member this.Dispose() = dispose(); GC.SuppressFinalize this
+    override __.Finalize() = dispose()
     interface ISerializable with
         member __.GetObjectData(info, _) = info.AddValue (secretName, secret)
