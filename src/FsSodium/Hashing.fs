6ef5130d77f65af172190adf739c361bc757adb8
Nikola Milekic <nikolamilekic@gmail.com>
2020-08-24 23:38:38 +0200

    Refactor Hashing to use FSharpPlus

--- src/FsSodium/Hashing.fs
+++ src/FsSodium/Hashing.fs
@@ -1,93 +1,89 @@
-module rec FsSodium.Hashing
+namespace FsSodium.Hashing
 
-open Milekic.YoLo
-open Extensions
+open FSharpPlus
+open FSharpPlus.Data
 
-#nowarn "40"
+open FsSodium
 
-module KeyLength =
-    let minimum = Interop.crypto_generichash_keybytes_min()
-    let maximum = Interop.crypto_generichash_keybytes_max()
-    let recommended =
-        Interop.crypto_generichash_keybytes()
-        |> KeyLength.Validate
-        |> Result.failOnError "Invalid recommended key length."
-    let getLength x = if isNull x then 0 else Array.length x
-type KeyLength =
-    private | KeyLength of int
-    static member Validate x =
-        validateRange KeyLength.minimum KeyLength.maximum KeyLength x
-    member this.Value = let (KeyLength x) = this in x
-type Key =
-    private | Key of byte[]
-    static member Generate (KeyLength x) =
-        (if x = 0 then null else Random.bytes x) |> Key
-    static member None = Key null
-    static member Validate x =
-        if isNull x then Key x |> Ok else
-        validateRange
-            KeyLength.minimum
-            KeyLength.maximum
-            (fun _ -> Key x)
-            (Array.length x)
-    member this.Value = let (Key x) = this in x
-
-module HashLength =
-    let minimum = Interop.crypto_generichash_bytes_min()
-    let maximum = Interop.crypto_generichash_bytes_max()
-    let recommended =
-        Interop.crypto_generichash_bytes()
-        |> HashLength.Validate
-        |> Result.failOnError "Invalid recommended hash length."
-type HashLength =
-    private | HashLength of int
-    static member Validate x =
-        validateRange HashLength.minimum HashLength.maximum HashLength x
+type HashingKeyLength = private | HashingKeyLength of int with
+    static member Minimum =
+        Interop.crypto_generichash_keybytes_min() |> HashingKeyLength
+    static member Maximum =
+        Interop.crypto_generichash_keybytes_max() |> HashingKeyLength
+    static member Recommended =
+        Interop.crypto_generichash_keybytes() |> HashingKeyLength
+    static member Custom x =
+        let result = HashingKeyLength x
+        if result < HashingKeyLength.Minimum || result > HashingKeyLength.Maximum
+        then Failure ()
+        else Success result
+    member this.Get = let (HashingKeyLength x) = this in x
+type HashingKey = private | HashingKey of byte[] with
+    static member Generate (HashingKeyLength x) =
+        (if x = 0 then null else Random.bytes x) |> HashingKey
+    static member None = HashingKey null
+    static member Create x =
+        if isNull x then HashingKey x |> Success else
+        Array.length x |> HashingKeyLength.Custom |>> konst (HashingKey x)
+    member this.Get = let (HashingKey x) = this in x
+type HashLength = private | HashLength of int with
+    static member Minimum =
+        Interop.crypto_generichash_bytes_min() |> HashLength
+    static member Maximum =
+        Interop.crypto_generichash_bytes_max() |> HashLength
+    static member Recommended =
+        Interop.crypto_generichash_bytes() |> HashLength
+    static member Custom x =
+        let result = HashLength x
+        if result < HashLength.Minimum || result > HashLength.Maximum
+        then Failure ()
+        else Success result
     member this.Value = let (HashLength x) = this in x
-
-type State =
-    private | State of byte[] * int
-    static member Make(Key k, HashLength hashLength) =
+type HashingState = private | HashingState of state:byte[] * hashLength:int with
+    static member Create(HashingKey k, HashLength hashLength) =
         let state = Array.zeroCreate 361
+        let keyLength = if isNull k then 0 else Array.length k
         let result =
             Interop.crypto_generichash_init(
                 state,
                 k,
-                uint64 (KeyLength.getLength k),
+                uint64 keyLength,
                 uint64 hashLength)
         if result = 0
-        then Ok <| State (state, hashLength)
+        then Ok <| HashingState (state, hashLength)
         else Error (SodiumError result)
-    member internal this.Value = let (State (bytes, length)) = this
-                                 bytes, length
+    member internal this.Get =
+        let (HashingState (bytes, length)) = this in bytes, length
 
-let hashPart (state : State) input =
-    let inputLength = Array.length input
-    let result =
-        Interop.crypto_generichash_update(
-            fst state.Value,
-            input,
-            uint64 inputLength)
-    if result = 0 then Ok () else Error <| SodiumError result
-let completeHash (state : State) =
-    let state, hashLength = state.Value
-    let hash = Array.zeroCreate hashLength
-    let result =
-        Interop.crypto_generichash_final(
-            state,
-            hash,
-            uint64 hashLength)
-    if result = 0 then Ok hash else Error <| SodiumError result
-
-let hash (Key key) (HashLength hashLength) input =
-    let inputLength = Array.length input
-    let hash = Array.zeroCreate hashLength
-    let result =
-        Interop.crypto_generichash(
-            hash,
-            uint64 hashLength,
-            input,
-            uint64 inputLength,
-            key,
-            uint64 (KeyLength.getLength key))
-    if result = 0 then Ok hash else Error <| SodiumError result
+[<RequireQualifiedAccess>]
+module Hashing =
+    let hashPart (state : HashingState) input =
+        let inputLength = Array.length input
+        let result =
+            Interop.crypto_generichash_update(
+                fst state.Get,
+                input,
+                uint64 inputLength)
+        if result = 0 then Ok () else Error <| SodiumError result
+    let completeHash (state : HashingState) =
+        let state, hashLength = state.Get
+        let hash = Array.zeroCreate hashLength
+        let result =
+            Interop.crypto_generichash_final(
+                state,
+                hash,
+                uint64 hashLength)
+        if result = 0 then Ok hash else Error <| SodiumError result
+    let hash (HashingKey key) (HashLength hashLength) input =
+        let inputLength = Array.length input
+        let hash = Array.zeroCreate hashLength
+        let keyLength = if isNull key then 0 else Array.length key
+        let result =
+            Interop.crypto_generichash(
+                hash,
+                uint64 hashLength,
+                input,
+                uint64 inputLength,
+                key,
+                uint64 keyLength)
+        if result = 0 then Ok hash else Error <| SodiumError result
--- tests/FsSodium.Tests/HashingTests.fs
+++ tests/FsSodium.Tests/HashingTests.fs
@@ -3,6 +3,7 @@ module FsSodium.Tests.HashingTests
 open Expecto
 open Swensen.Unquote
 open Milekic.YoLo
+open FSharpPlus
 
 open FsSodium.Hashing
 
@@ -14,102 +15,118 @@ let hashingTests =
         testList "All at once" [
             testCase "Same input produces same hash with no key" <| fun _ ->
                 let input = [| 1uy; 2uy; 3uy |]
-                hash (Key.None) HashLength.recommended input
-                =! hash (Key.None) HashLength.recommended input
+                Hashing.hash (HashingKey.None) HashLength.Recommended input
+                =! Hashing.hash (HashingKey.None) HashLength.Recommended input
             testCase "Same input produces same hash with key" <| fun _ ->
                 let input = [| 1uy; 2uy; 3uy |]
-                let keyLength = KeyLength.recommended
-                let key = Key.Generate keyLength
-                hash key HashLength.recommended input
-                =! hash key HashLength.recommended input
+                let keyLength = HashingKeyLength.Recommended
+                let key = HashingKey.Generate keyLength
+                Hashing.hash key HashLength.Recommended input
+                =! Hashing.hash key HashLength.Recommended input
             testCase "Different input produces different hash" <| fun _ ->
-                hash (Key.None) HashLength.recommended [| 1uy; 2uy; 3uy |]
-                <>! hash (Key.None) HashLength.recommended [| 1uy; 2uy |]
+                Hashing.hash
+                    (HashingKey.None) HashLength.Recommended [| 1uy; 2uy; 3uy |]
+                <>! Hashing.hash
+                    (HashingKey.None) HashLength.Recommended [| 1uy; 2uy |]
             testCase "Different key produces different hash" <| fun _ ->
-                let keyLength = KeyLength.recommended
-                let key1 = Key.Generate keyLength
-                let key2 = Key.Generate keyLength
+                let keyLength = HashingKeyLength.Recommended
+                let key1 = HashingKey.Generate keyLength
+                let key2 = HashingKey.Generate keyLength
                 let input = [| 1uy; 2uy; 3uy |]
-                hash key1 HashLength.recommended input
-                <>! hash key2 HashLength.recommended input
+                Hashing.hash key1 HashLength.Recommended input
+                <>! Hashing.hash key2 HashLength.Recommended input
         ]
         testList "Parts" [
             testCase "Same input produces same hash with no key" <| fun _ ->
                 let hash () =
-                    result {
-                        let! state = State.Make(Key.None, HashLength.recommended)
-                        let hashPart = hashPart state
+                    monad {
+                        let! state =
+                            HashingState.Create(
+                                HashingKey.None, HashLength.Recommended)
+                        let hashPart = Hashing.hashPart state
                         do! hashPart [|1uy; 2uy|]
                         do! hashPart [|3uy; 4uy|]
                         do! hashPart [|5uy|]
-                        return! completeHash state
+                        return! Hashing.completeHash state
                     }
-                    |> Result.failOnError "Hashing failed"
+                    |> Result.get
                 hash () =! hash ()
             testCase "Same input produces same hash with key" <| fun _ ->
                 let hash key =
-                    result {
-                        let! state = State.Make(key, HashLength.recommended)
-                        let hashPart = hashPart state
+                    monad {
+                        let! state =
+                            HashingState.Create(
+                                key, HashLength.Recommended)
+                        let hashPart = Hashing.hashPart state
                         do! hashPart [|1uy; 2uy|]
                         do! hashPart [|3uy; 4uy|]
                         do! hashPart [|5uy|]
-                        return! completeHash state
+                        return! Hashing.completeHash state
                     }
-                    |> Result.failOnError "Hashing failed"
-                let keyLength = KeyLength.recommended
-                let key = Key.Generate keyLength
+                    |> Result.get
+                let keyLength = HashingKeyLength.Recommended
+                let key = HashingKey.Generate keyLength
                 hash key =! hash key
             testCase "Different input produces different hash" <| fun _ ->
                 let hash firstPart =
-                    result {
-                        let! state = State.Make(Key.None, HashLength.recommended)
-                        let hashPart = hashPart state
+                    monad {
+                        let! state =
+                            HashingState.Create(
+                                HashingKey.None, HashLength.Recommended)
+                        let hashPart = Hashing.hashPart state
                         do! hashPart firstPart
                         do! hashPart [|3uy; 4uy|]
                         do! hashPart [|5uy|]
-                        return! completeHash state
+                        return! Hashing.completeHash state
                     }
-                    |> Result.failOnError "Hashing failed"
+                    |> Result.get
                 hash [|1uy; 2uy|] <>! hash [|1uy|]
             testCase "Different key produces different hash" <| fun _ ->
                 let hash key =
-                    result {
-                        let! state = State.Make(key, HashLength.recommended)
-                        let hashPart = hashPart state
+                    monad {
+                        let! state =
+                            HashingState.Create(key, HashLength.Recommended)
+                        let hashPart = Hashing.hashPart state
                         do! hashPart [|1uy; 2uy|]
                         do! hashPart [|3uy; 4uy|]
                         do! hashPart [|5uy|]
-                        return! completeHash state
+                        return! Hashing.completeHash state
                     }
-                    |> Result.failOnError "Hashing failed"
-                let keyLength = KeyLength.recommended
-                let key1 = Key.Generate keyLength
-                let key2 = Key.Generate keyLength
+                    |> Result.get
+                let keyLength = HashingKeyLength.Recommended
+                let key1 = HashingKey.Generate keyLength
+                let key2 = HashingKey.Generate keyLength
                 hash key1 <>! hash key2
             testCase "Produces same hash as all at once API" <| fun _ ->
                 let hashPart1 =
-                    result {
-                        let! state = State.Make(Key.None, HashLength.recommended)
-                        let hashPart = hashPart state
+                    monad {
+                        let! state =
+                            HashingState.Create(
+                                HashingKey.None, HashLength.Recommended)
+                        let hashPart = Hashing.hashPart state
                         do! hashPart [|1uy; 2uy|]
                         do! hashPart [|3uy; 4uy|]
                         do! hashPart [|5uy|]
-                        return! completeHash state
+                        return! Hashing.completeHash state
                     }
-                    |> Result.failOnError "Hashing failed"
+                    |> Result.get
                 let hashPart2 =
-                    result {
-                        let! state = State.Make(Key.None, HashLength.recommended)
-                        let hashPart = hashPart state
+                    monad {
+                        let! state =
+                            HashingState.Create(
+                                HashingKey.None, HashLength.Recommended)
+                        let hashPart = Hashing.hashPart state
                         do! hashPart [|1uy; 2uy; 3uy|]
                         do! hashPart [|4uy; 5uy|]
-                        return! completeHash state
+                        return! Hashing.completeHash state
                     }
-                    |> Result.failOnError "Hashing failed"
+                    |> Result.get
                 let allAtOnceHash =
-                    hash (Key.None) HashLength.recommended [|1uy; 2uy; 3uy; 4uy; 5uy|]
-                    |> Result.failOnError "Hashing failed"
+                    Hashing.hash
+                        HashingKey.None
+                        HashLength.Recommended
+                        [|1uy; 2uy; 3uy; 4uy; 5uy|]
+                    |> Result.get
                 hashPart1 =! hashPart2
                 hashPart1 =! allAtOnceHash
         ]
