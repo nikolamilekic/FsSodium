0e8918bc5e4c4bb2900f851b811bf3d778f8e7df
Nikola Milekic <nikolamilekic@gmail.com>
2020-08-26 23:00:13 +0200

    Refactor PublicKeyAuthentication

--- src/FsSodium/PublicKeyAuthentication.fs
+++ src/FsSodium/PublicKeyAuthentication.fs
@@ -1,14 +1,17 @@
-module FsSodium.PublicKeyAuthentication
+namespace FsSodium.PublicKeyAuthentication
 
 open System
+open FsSodium
 
-let private macLength = Interop.crypto_sign_bytes() |> capToInt
-let private publicKeyLength = Interop.crypto_sign_publickeybytes() |> capToInt
-let private secretKeyLength = Interop.crypto_sign_secretkeybytes() |> capToInt
+module internal AlgorithmInfo =
+    let macLength = Interop.crypto_sign_bytes() |> int
+    let publicKeyLength = Interop.crypto_sign_publickeybytes() |> int
+    let secretKeyLength = Interop.crypto_sign_secretkeybytes() |> int
+
+open AlgorithmInfo
 
 type SecretKey private (secretKey) =
     inherit Secret(secretKey)
-    static member Length = secretKeyLength
     static member Generate() =
         let publicKey = Array.zeroCreate publicKeyLength
         let secretKey = Array.zeroCreate secretKeyLength
@@ -16,46 +19,45 @@ type SecretKey private (secretKey) =
         let result = Interop.crypto_sign_keypair(publicKey, secretKey)
         if result = 0 then Ok (secret, PublicKey publicKey)
         else (secret :> IDisposable).Dispose(); Error <| SodiumError result
-    static member Validate x =
-        validateArrayLength secretKeyLength (fun x -> new SecretKey(x)) x
-and PublicKey =
-    private | PublicKey of byte[]
-    static member Length = publicKeyLength
-    static member Validate x =
-        validateArrayLength publicKeyLength PublicKey x
-    static member Compute (secretKey : SecretKey) =
+    static member Import x =
+        if Array.length x <> secretKeyLength
+        then Error ()
+        else Ok <| new SecretKey(x)
+and PublicKey = private | PublicKey of byte[] with
+    static member Import x =
+        if Array.length x <> publicKeyLength then Error () else Ok <| PublicKey x
+    static member FromSecretKey (secretKey : SecretKey) =
         let publicKey = Array.zeroCreate publicKeyLength
         let result =
             Interop.crypto_sign_ed25519_sk_to_pk(publicKey, secretKey.Get)
         if result = 0 then Ok <| PublicKey publicKey
         else Error <| SodiumError result
-    member this.Value = let (PublicKey x) = this in x
+    member this.Get = let (PublicKey x) = this in x
+type Mac = private | Mac of byte[] with
+    static member Import x =
+        if Array.length x <> macLength then Error () else Ok <| Mac x
+    member this.Get = let (Mac x) = this in x
 
-type Mac =
-    private | Mac of byte[]
-    static member Length = macLength
-    static member Validate x = validateArrayLength macLength Mac x
-    member this.Value = let (Mac x) = this in x
-
-let sign (secretKey : SecretKey) message =
-    let messageLength = Array.length message
-    let mac = Array.zeroCreate macLength
-    let result =
-        Interop.crypto_sign_detached(
-            mac,
-            IntPtr.Zero,
-            message,
-            uint64 messageLength,
-            secretKey.Get)
-    if result = 0 then Ok <| Mac mac
-    else Error <| SodiumError result
-
-let verify (PublicKey key) message (Mac mac) =
-    let messageLength = Array.length message
-    let result =
-        Interop.crypto_sign_verify_detached(
-            mac,
-            message,
-            uint64 messageLength,
-            key)
-    if result = 0 then Ok () else Error <| SodiumError result
+[<RequireQualifiedAccess>]
+module PublicKeyAuthentication =
+    let sign (secretKey : SecretKey) message =
+        let messageLength = Array.length message
+        let mac = Array.zeroCreate macLength
+        let result =
+            Interop.crypto_sign_detached(
+                mac,
+                IntPtr.Zero,
+                message,
+                uint64 messageLength,
+                secretKey.Get)
+        if result = 0 then Ok <| Mac mac
+        else Error <| SodiumError result
+    let verify (PublicKey key) (message, (Mac mac)) =
+        let messageLength = Array.length message
+        let result =
+            Interop.crypto_sign_verify_detached(
+                mac,
+                message,
+                uint64 messageLength,
+                key)
+        if result = 0 then Ok () else Error <| SodiumError result
--- tests/FsSodium.Tests/PublicKeyAuthenticationTests.fs
+++ tests/FsSodium.Tests/PublicKeyAuthenticationTests.fs
@@ -3,42 +3,53 @@ module FsSodium.Tests.PublicKeyAuthenticationTests
 open Expecto
 open Swensen.Unquote
 open Milekic.YoLo
-open Milekic.YoLo.Result.Operators
+open FSharpPlus
+
 open FsSodium
-open PublicKeyAuthentication
+open FsSodium.PublicKeyAuthentication
 
 do initializeSodium()
 
 let secretKey, publicKey =
     SecretKey.Generate() |> Result.failOnError "Key generation failed"
-let signWithFixture = sign secretKey >> Result.failOnError "Signing failed"
-let verifyWithFixture = verify publicKey
 
 [<Tests>]
 let publicKeyAuthenticationTests =
     testList "PublicKeyAuthenticaion" [
         yield testCase "Verify works for unmodified message" <| fun () ->
             let plainText = [|1uy; 2uy; 3uy|]
-            let mac = signWithFixture plainText
-            verifyWithFixture plainText mac =! Ok ()
+            let mac =
+                PublicKeyAuthentication.sign secretKey plainText
+                |> Result.failOnError "Signing failed"
+            PublicKeyAuthentication.verify publicKey (plainText, mac) =! Ok ()
         yield testCase "Verify fails for modified signature" <| fun () ->
             let plainText = [|1uy; 2uy; 3uy|]
-            let mac = (signWithFixture plainText).Value
-            mac.[0] <- if mac.[0] = 0uy then 1uy else 0uy
-            let mac = Mac.Validate mac |> Result.failOnError "Bad mac"
-            verifyWithFixture plainText mac =! (Error <| SodiumError -1)
+            let rawMac =
+                PublicKeyAuthentication.sign secretKey plainText
+                |>> fun m -> m.Get
+                |> Result.failOnError "Signing failed"
+            rawMac.[0] <- if rawMac.[0] = 0uy then 1uy else 0uy
+            let mac = Mac.Import rawMac |> Result.failOnError "Could not reimport mac"
+            PublicKeyAuthentication.verify publicKey (plainText, mac)
+            =! (Error <| SodiumError -1)
         yield testCase "Verify fails for modified message" <| fun () ->
             let plainText = [|1uy; 2uy; 3uy|]
-            let mac = signWithFixture plainText
-            plainText.[0] <- if plainText.[0] = 0uy then 1uy else 0uy
-            verifyWithFixture plainText mac =! (Error <| SodiumError -1)
+            let mac =
+                PublicKeyAuthentication.sign secretKey plainText
+                |> Result.failOnError "Signing failed"
+            plainText.[0] <- 0uy
+            PublicKeyAuthentication.verify publicKey (plainText, mac)
+            =! (Error <| SodiumError -1)
         yield testCase "Verify fails for wrong key" <| fun () ->
             let plainText = [|1uy; 2uy; 3uy|]
-            let mac = signWithFixture plainText
+            let mac =
+                PublicKeyAuthentication.sign secretKey plainText
+                |> Result.failOnError "Signing failed"
             let pkEve =
-                SecretKey.Generate() >>- snd
+                SecretKey.Generate() |>> snd
                 |> Result.failOnError "Eve key generation failed"
-            verify pkEve plainText mac =! (Error <| SodiumError -1)
+            PublicKeyAuthentication.verify pkEve (plainText, mac)
+            =! (Error <| SodiumError -1)
         yield testCase "Public key computation from secret key works" <| fun () ->
-            PublicKey.Compute secretKey =! Ok publicKey
+            PublicKey.FromSecretKey secretKey =! Ok publicKey
     ]
