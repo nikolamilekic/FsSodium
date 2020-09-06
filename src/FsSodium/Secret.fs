5eee44754e85cbf8a114dc36491ac7a0648fe04a
Nikola Milekic <nikolamilekic@gmail.com>
2020-09-06 18:16:07 +0200

    Remove ISerializable implementation from Secret
    Was causing problems as some serialization frameworks (e.g. FsPickler)
    expect all inherited classes to implement the deserialization ctor as
    well.
    
    Secrets should not be serialized just like that anyway so removing this
    is ok.

--- src/FsSodium/Secret.fs
+++ src/FsSodium/Secret.fs
@@ -1,18 +1,12 @@
 namespace FsSodium
 
 open System
-open System.Runtime.Serialization
-open Milekic.YoLo
 
 type Secret(secret) =
-    static let secretName = nameOf <@ instanceOf<Secret>.Get @>
     let secretLength = Array.length secret |> uint32
     do
         Sodium.initialize ()
         Interop.sodium_mlock(secret, secretLength) |> ignore
-    new(info : SerializationInfo, _ : StreamingContext) =
-        let secret = info.GetValue (secretName, typeof<byte[]>) :?> byte[]
-        new Secret(secret)
     abstract member Dispose : unit -> unit
     default __.Dispose() =
         Interop.sodium_munlock(secret, secretLength) |> ignore
@@ -20,5 +14,3 @@ type Secret(secret) =
     interface IDisposable with
         member this.Dispose() = this.Dispose(); GC.SuppressFinalize this
     override this.Finalize() = this.Dispose()
-    interface ISerializable with
-        member __.GetObjectData(info, _) = info.AddValue (secretName, secret)
