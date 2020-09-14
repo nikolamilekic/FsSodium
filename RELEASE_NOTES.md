541de96d96e86e7f048990455789b097f5533260
Nikola Milekic <nikolamilekic@gmail.com>
2020-09-14 15:39:06 +0200

    Make build script more generic and bump version

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,4 +1,4 @@
-### 0.3-dev - Unreleased
+### 0.3 (Released 2020/09/14)
 * Hashing (both generic and SHA512)
 * Common.getSodiumVersion
 * SecretKeyAuthentication
@@ -7,7 +7,7 @@
 * XSalsa20
 * Key derivation
 
-### 0.2 - 2019/07/30
+## New in 0.2 (Released 2019/07/30)
 * Switched to using Milekic.YoLo library
 * PublicKeyEncryption.encrypt now returns nonce in addition to cipher text to simplify interface usage
 * Added Random
@@ -17,8 +17,8 @@
 * Added 'public key from secret key' computation for both public key authentication and encryption
 * Updated FSharp.Core references as per guidelines
 
-### 0.1.1 - 2018/04/25
+## New in 0.1.1 (Released 2018/04/25)
 * Moved YoLo to FsSodium namespace
 
-### 0.1 - 2018/04/22
+## New in 0.1 (Released 2018/04/22)
 * Added PublicKeyAuthentication and PublicKeyEncryption
--- build.fsx
+++ build.fsx
@@ -20,7 +20,7 @@ module FinalVersion =
 
     let pathToAssemblyInfoFile =
         lazy
-        !! "src/FsSodium/obj/Release/**/FsSodium.AssemblyInfo.fs"
+        !! "src/*/obj/Release/**/*.AssemblyInfo.fs"
         |> Seq.head
 
     let (|Regex|_|) pattern input =
@@ -69,15 +69,17 @@ module Clean =
 module Build =
     //nuget Fake.DotNet.Cli
     //nuget Fake.BuildServer.AppVeyor
+    //nuget Fake.IO.FileSystem
 
     open Fake.DotNet
     open Fake.Core
     open Fake.Core.TargetOperators
     open Fake.BuildServer
+    open Fake.IO.Globbing.Operators
 
     open FinalVersion
 
-    let projectToBuild = "FsSodium.sln"
+    let projectToBuild = !! "*.sln" |> Seq.head
 
     Target.create "Build" <| fun _ ->
         DotNet.build id projectToBuild
@@ -98,14 +100,16 @@ module Build =
 
 module Pack =
     //nuget Fake.DotNet.Cli
+    //nuget Fake.IO.FileSystem
 
     open Fake.DotNet
     open Fake.Core
+    open Fake.IO.Globbing.Operators
 
     open CustomTargetOperators
     open ReleaseNotesParsing
 
-    let projectToPack = "FsSodium.sln"
+    let projectToPack = !! "*.sln" |> Seq.head
 
     Target.create "Pack" <| fun _ ->
         let newBuildProperties = [ "PackageReleaseNotes", releaseNotes.Value ]
@@ -171,6 +175,7 @@ module UploadArtifactsToGitHub =
     //nuget Fake.IO.FileSystem
     //nuget Fake.BuildServer.AppVeyor
 
+    open System.IO
     open Fake.Core
     open Fake.Api
     open Fake.IO.Globbing.Operators
@@ -180,7 +185,7 @@ module UploadArtifactsToGitHub =
     open FinalVersion
     open ReleaseNotesParsing
 
-    let productName = "FsSodium"
+    let productName = !! "*.sln" |> Seq.head |> Path.GetFileNameWithoutExtension
     let gitOwner = "nikolamilekic"
 
     Target.create "UploadArtifactsToGitHub" <| fun _ ->
@@ -226,17 +231,36 @@ module UploadPackageToNuget =
 module Release =
     //nuget Fake.Tools.Git
 
+    open System.Text.RegularExpressions
+    open Fake.IO
+    open Fake.IO.Globbing.Operators
     open Fake.Core
     open Fake.Tools
 
     open CustomTargetOperators
 
-    let gitHome = "git@github.com:nikolamilekic/FsSodium.git"
+    let pathToThisAssemblyFile =
+        lazy
+        !! "src/*/obj/Release/**/ThisAssembly.GitInfo.g.fs"
+        |> Seq.head
+
+    let (|Regex|_|) pattern input =
+        let m = Regex.Match(input, pattern)
+        if m.Success then Some(List.tail [ for g in m.Groups -> g.Value ])
+        else None
+
+    let gitHome =
+        lazy
+        pathToThisAssemblyFile.Value
+        |> File.readAsString
+        |> function
+            | Regex "RepositoryUrl = @\"(.+)\"" [ gitHome ] -> gitHome
+            | _ -> failwith "Could not parse this assembly file"
 
     Target.create "Release" <| fun _ ->
         Git.CommandHelper.directRunGitCommandAndFail
             ""
-            (sprintf "push -f %s HEAD:release" gitHome)
+            (sprintf "push -f %s HEAD:release" gitHome.Value)
 
     [ "Clean"; "Build"; "Test" ] ==> "Release"
 
@@ -246,7 +270,7 @@ module AppVeyor =
     open CustomTargetOperators
 
     Target.create "AppVeyor" ignore
-    [ "UploadArtifactsToGitHub"; "UploadPackageToNuget" ]
+    [ "UploadArtifactsToGitHub" ]
     ==> "AppVeyor"
 
 module Default =
